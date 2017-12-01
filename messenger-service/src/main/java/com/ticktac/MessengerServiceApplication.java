54d56591c17e45825b5b76cfbe99d7bf269e6bc5
KostasTzouvalidis <kostastzouvalidis@gmail.com>
2017-12-01 18:23:14 +0100

    Add messaging service starter project

---
+++ messenger-service/src/main/java/com/ticktac/MessengerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ticktac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerServiceApplication.class, args);
+	}
+}
---
+++ messenger-service/src/test/java/com/ticktac/MessengerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ticktac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessengerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
