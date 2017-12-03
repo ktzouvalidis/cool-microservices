2a1a6efebcd24c8dc7168d292d61131c0eb925be
unknown <nnystad@gmail.com>
2017-12-03 22:06:41 +0100

    let bank microservice accept card number

--- bank-service/BankMicroservice/src/main/java/es/uc3m/tiw/bank/Bank.java
+++ bank-service/BankMicroservice/src/main/java/es/uc3m/tiw/bank/Bank.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class Bank {
 	@RequestMapping(value = "/banking", method = RequestMethod.GET)
-	public @ResponseBody String bankingService() {
-		return "This string is provided by the Bank Microservice. Good job";
+	public @ResponseBody String bankingService(@RequestParam(value = "cardNumber", required = true) int cardNumber) {
+		
+		return "From Bank Microservice. Card number entered: " + cardNumber;
 	}
 }
