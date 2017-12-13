600909be3d469a416dcd6602e4e5a76d9a085c29
KostasTzouvalidis <kostastzouvalidis@gmail.com>
2017-12-13 16:07:24 +0100

    Add sendername column at Message

--- messenger-service/src/main/java/com/ticktac/MessengerServiceApplication.java
+++ messenger-service/src/main/java/com/ticktac/MessengerServiceApplication.java
@@ -1,37 +1,10 @@
 package com.ticktac;
 
-import javax.jms.ConnectionFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 
 @SpringBootApplication
 public class MessengerServiceApplication {
-	
-	/*@Bean
-	public JmsListenerContainerFactory<?> ticktacCF(ConnectionFactory connectionFactory,
-			DefaultJmsListenerContainerFactoryConfigurer configurer) {
-		DefaultJmsListenerContainerFactory cf = new DefaultJmsListenerContainerFactory();
-		configurer.configure(cf, connectionFactory);
-		
-		return cf;
-	}
-	
-	@Bean
-	public MessageConverter jacksonJmsMessageConverter() {
-		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-		converter.setTargetType(MessageType.TEXT);
-		converter.setTypeIdPropertyName("_type");
-		
-		return converter;
-	}*/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MessengerServiceApplication.class, args);
